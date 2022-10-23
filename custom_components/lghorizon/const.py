894b85c7bcb8386e0bdea7679708eef79ada1cef
Rudolf Offereins <r.offereins@gmail.com>
2022-10-23 03:27:23 +0200

    :sparkles: Update HA en custom components

--- custom_components/lghorizon/const.py
+++ custom_components/lghorizon/const.py
@@ -12,12 +12,12 @@ REMOTE_KEY_PRESS = "remote_key_press"
 
 COUNTRY_CODES = {
     "Ziggo": "nl",
-    # "Telenet (BE)": "be-nl",
+    "Telenet (BE)": "be-nl",
     # "Telenet (BE, PREPROD)": "be-nl-preprod",
     # "Telenet (FR)": "be-fr",
-    # "Magenta": "at",
-    # "UPC Switzerland": "ch",
+    "Magenta": "at",
+    "UPC Switzerland": "ch",
     # "Virgin Media (GB)": "gb",
     # "Virgin Media (GB, PREPROD)": "gb-preprod",
-    # "Virgin Media (IE)": "ie",
+    "Virgin Media (IE)": "ie",
 }
